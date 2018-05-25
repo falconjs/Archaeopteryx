6c794604b3be42c838fd52fc38c9dabb598d9fc6
James Bai <12628503+falconjs@users.noreply.github.com>
2018-05-25 14:34:59 +0800

    up to 24 partial dependence plots

--- Learning/Pandas/pandas.DataFrame.py
+++ Learning/Pandas/pandas.DataFrame.py
@@ -62,3 +62,53 @@ pd.concat([df1, df4], axis=1)
 #   letter  number  animal    name
 # 0      a       1    bird   polly
 # 1      b       2  monkey  george
+
+
+
+df5 = pd.DataFrame([['a', 1], ['b', 2]], columns=['letter1', 'number1'], index=[0,1])
+
+#   letter1  number1
+# 0      a       1
+# 1      b       2
+
+df6 = pd.DataFrame([['c', 3], ['d', 4]], columns=['letter1', 'number2'], index=[1,2])
+
+#   letter1  number2
+# 1      c       3
+# 2      d       4
+
+(df7, df8) = df5.align(df6, axis=0)
+
+#   letter1  number1
+# 0       a      1.0
+# 1       b      2.0
+# 2     NaN      NaN
+
+#   letter1  number2
+# 0     NaN      NaN
+# 1       c      3.0
+# 2       d      4.0
+
+(df9, df10) = df5.align(df6, axis=1)
+
+#   letter1  number1  number2
+# 0       a        1      NaN
+# 1       b        2      NaN
+
+#   letter1  number1  number2
+# 1       c      NaN        3
+# 2       d      NaN        4
+
+df5.align(df6)
+
+ #   letter1  number1  number2
+ # 0       a      1.0      NaN
+ # 1       b      2.0      NaN
+ # 2     NaN      NaN      NaN
+
+ #   letter1  number1  number2
+ # 0     NaN      NaN      NaN
+ # 1       c      NaN      3.0
+ # 2       d      NaN      4.0
+
+df5.align(df6, axis=1, join='left')
---
+++ Learning/XGBoost/xgboost_install_validation.py
@@ -0,0 +1,3 @@
+# https://machinelearningmastery.com/gentle-introduction-xgboost-applied-machine-learning/
+
+# from xgboost import XGBRegressor
