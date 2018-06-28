d0fed240b843dfc0c09ddf77ebd36835c728e356
James Bai <12628503+falconjs@users.noreply.github.com>
2018-06-28 17:41:43 +0800

    20180628

---
+++ Learning/Numpy/list on numpy.py
@@ -0,0 +1,12 @@
+# ==========================================================
+
+import numpy as np
+
+scores = [3.0, 1.0, 0.2]
+
+"""Compute softmax values for each sets of scores in x."""
+np.exp(scores)
+# array([20.08553692,  2.71828183,  1.22140276])
+
+
+
