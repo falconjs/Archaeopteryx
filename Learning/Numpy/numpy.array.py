25b5173ea9dd6c46020e5212564779fd95def900
James Bai <12628503+falconjs@users.noreply.github.com>
2018-07-12 16:43:06 +0800

    20180712

--- Learning/Numpy/numpy.array.py
+++ Learning/Numpy/numpy.array.py
@@ -13,6 +13,29 @@ a.argsort()[-3:]
 a.argsort()[-3:][::-1]
 # array([5, 2, 4], dtype=int64)
 
+
+# array[start:end:step]
+
+a[0:]
+# array([1, 3, 5, 2, 4, 6])
+a[3:]
+# array([2, 4, 6])
+a[-1:]
+# array([6])
+a[:1]
+# array([1])
+a[:2]
+# array([1, 3])
+a[2:2]
+# array([], dtype=int32)
+
+a[[1,3,4]]
+# array([3, 2, 4])
+
+a[:-1]
+# array([1, 3, 5, 2, 4])
+
+
 # array[<start point>::<step>]
 a[0::]
 # array([1, 3, 5, 2, 4, 6])
@@ -27,7 +50,6 @@ a[::2]
 a[::-1]
 # array([6, 4, 2, 5, 3, 1])
 
-
 X = np.array([[1., 2., 3.],
               [4., 5., 6.],
               [7., 8., 9.]])
@@ -38,7 +60,24 @@ X[0]
 X[:,0]
 # array([1., 4., 7.])
 
+# ================= Search, index, position ==================
 
+np.argwhere(a == 5)
+# array([[2]], dtype=int64)
+
+np.argwhere(a > 4)
+# array([[2],
+#        [5]], dtype=int64)
+
+
+# ==================== show , print, string ==================
+
+a = np.array([1, 2, 3], int)
+np.array2string(a, precision=2, separator=',')
+
+b = np.array([[1], [2], [3]], int)
+np.array_str(b)
+np.array2string(b, precision=2, separator=',')
 
 # ===================== Math ==========================
 X1 = np.array([[1., 2., 3.],
