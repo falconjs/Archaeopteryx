07f3c294f53f509e4790438c46c0d1929ac77682
James Bai <12628503+falconjs@users.noreply.github.com>
2018-07-12 16:43:06 +0800

    20180712

---
+++ Learning/Basic/Python - Pickle.py
@@ -0,0 +1,46 @@
+import os
+import pickle
+
+example_dict = {1:"d", 2:"2", 3:"f"}
+
+root_dir = "./Learning/Basic"
+pickle_file = os.path.join(root_dir, 'dict.pickle')
+
+# ===================== Write ======================
+
+pickle_out = open(pickle_file, "wb")
+pickle.dump(example_dict, pickle_out)
+pickle_out.close()
+
+
+pickle_in = open(pickle_file, "rb")
+example_dict2 = pickle.load(pickle_in)
+pickle_in.close()
+
+
+example_dt1 = {1:"d", 2:"e", 3:"f"}
+example_dt2 = {4:"s", 5:"d", 6:"g"}
+example_dt3 = {7:"u", 8:"i", 9:"o"}
+
+f = open(pickle_file, 'wb')
+save = {
+    'example_dt1': example_dt1,
+    'example_dt2': example_dt2,
+    'example_dt3': example_dt3,
+}
+pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+f.close()
+
+# ====================== Read =======================
+
+pickle_in = open(pickle_file, "rb")
+example_dict2 = pickle.load(pickle_in)
+pickle_in.close()
+
+example_dt4 = example_dict2['example_dt1']
+
+
+# try
+# except Exception as e:
+#     print('Unable to save data to', pickle_file, ':', e)
+#     raise
---
+++ Learning/Basic/Python - print.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = 2
+b = np.array([[1], [2], [3], [4]])
+
+print("valid_dataset[%s] ~ \ntrain_dataset: \n%s" % (a, b))
+
+# Out:
+# valid_dataset[2] ~
+# train_dataset:
+# [[1]
+#  [2]
+#  [3]
+#  [4]]
+
