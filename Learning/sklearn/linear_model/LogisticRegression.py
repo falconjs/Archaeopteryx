8c01828aac120d3e64c665464c1f2c5a966d343d
James Bai <12628503+falconjs@users.noreply.github.com>
2018-07-23 17:01:39 +0800

    20180723

---
+++ Learning/sklearn/linear_model/LogisticRegression.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+
+X_dataset = np.array([
+     [1., 2., 3.],
+     [4., 5., 6.],
+     [7., 8., 9.],
+     [11., 12., 13],
+     [14., 15., 16.],
+     [17., 18., 19.],
+     [21., 22., 23.],
+     [24., 25., 26.],
+     [27., 28., 29.]
+]
+)
+
+y_dataset = np.array([
+     [1, 0, 0],
+     [1, 0, 0],
+     [1, 0, 0],
+     [0, 1, 0],
+     [0, 1, 0],
+     [0, 1, 0],
+     [0, 0, 1],
+     [0, 0, 1],
+     [0, 0, 1]
+]
+)
+
+logistic_reg = LogisticRegression()
+
+logistic_reg.fit(X_dataset, y_dataset)
+# ValueError: bad input shape (9, 3)
+# y : array-like, shape (n_samples,)
+
